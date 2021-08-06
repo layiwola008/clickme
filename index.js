48a8d3569d135b99a4f2b20431003c39f3dfb0ff
Olayiwola Adepoju <78272392+layiwola008@users.noreply.github.com>
2021-08-06 11:45:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function handleClicks() {
+  let clickCount = 0;
+  $('.js-click-counter').text(clickCount);
+  
+  $('.js-clicker').click(function(event) {
+    clickCount += 1;
+    $('.js-click-counter').text(clickCount);
+  });
+}
+
+$(handleClicks);
